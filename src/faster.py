b4384f76f5e8640661d947ba7cd552c86fe1daea
Shamith Pasula <shamith09@berkeley.edu>
2022-03-22 17:00:22 -0700

    Refactored

--- src/faster.py
+++ src/faster.py
@@ -10,126 +10,32 @@ from utils import *
 # Default comments
 default_comments = ['Calculation error', 'Misread question']
 
-print('\nHello! My name is 16Bot, made by a lazy CS & Physics major named Shamith Pasula.')
-print('I will be doing your 16A or 16B self-grade for you, giving you 8/10 on random questions to not be sus.')
-print('The comments for the 8/10 questions are in data.json, edit them if you wish.')
-print('If this isn\'t your first time meeting me and you want to update your data, delete data.json and run faster.py again.')
-print('If you mess up or want to restart, press Ctrl+C and run faster.py again. \n')
+greet()
+data_dict = ask_user(default_comments)
+more_qs(data_dict)
+parts = get_inputs(data_dict)
 
-# params
-try:
-    data_dict = open('data.json', 'r')
-    data_dict = json.load(data_dict)
-    if 'comments' not in data_dict:
-        data_dict['comments'] = default_comments
-        with open('data.json', 'w') as data:
-            data.write(json.dumps(data_dict))
-    if 'class' not in data_dict:
-        print('Which class are you taking? Enter the number corresponding to your answer:')
-        print('1) EECS 16A')
-        print('2) EECS 16B')
-        data_dict['class'] = '16A' if input('Enter answer here: ').strip() == '1' else '16B'
-        with open('data.json', 'w') as data:
-            data.write(json.dumps(data_dict))
-    print('\nI have your name, email, and SID already!\n')
-except:
-    while True:
-        try:
-            print('I have a few questions for you:\n')
-            get_data(data_dict, default_comments)
-            break
-        except ValueError:
-            print(red + '\nERROR: Bad input. Restarting.\n' + end)
+shuffle(parts)
 
-while True:
-    try:
-        hw_number = int(input('Which homework are you self-grading? ').strip())
-        if hw_number < 10:
-            hw_number = '0' + str(hw_number)
-        else:
-            hw_number = str(hw_number)
+print(f'There are {len(parts)} questions on this HW.')
+print()
 
-        if data_dict['class'] == '16b':
-            resubmission = 'no'
-            while (resubmission != 'Y' and resubmission != 'N'):
-                resubmission = input('Is this a resubmission (Enter Y or N)? ').strip().upper()
-            resubmission = 'yes' if resubmission == 'Y' else 'no'
+num_incorrects = int(input('How many questions out of these do you want to give an 8/10? ').strip())
 
-        difficulty = int(input(f'How difficult was HW {hw_number} (Enter an integer between 1-10)? ').strip())
-        hours_spent = int(input(f'How many hours did you spend on HW {hw_number}? ').strip())
-        print()
-        num_teammates = int(input('How many people did you work with? ').strip())
+comments = infinite_gen(data_dict['comments'])
+out_dict = data_dict.copy()
+out_dict.pop('class')
+out_dict.pop('comments')
 
-        went_to_hw_party = None
-        while (went_to_hw_party != 'Y' and went_to_hw_party != 'N'):
-            went_to_hw_party = input('Did you go to HW party (Enter Y or N)? ').strip().upper()
-        break
-    except ValueError:
-        print(red + '\nERROR: Bad input. Restarting.\n' + end)
+for p in parts[:-num_incorrects]:
+    out_dict['q' + p] = "10"
+
+for p in parts[-num_incorrects:]:
+    out_dict['q' + p] = "8"
+    out_dict[f'q{p}-comment'] = next(comments)
+    
+with open(f'out/selfgrades-{data_dict["hwNum"]}.json', 'w') as out:
+    out.write(json.dumps(out_dict))
 
 print()
-for i in range(5):
-    print(f'{cyan}A new Google Chrome window will open in a new window in {5 - i} seconds. I will navigate back to the terminal to ask you one last question.{end}', end = '\r')
-    sleep(1)
-print('\nOpening now...')
-
-with Browser('chrome') as browser:
-    difficulty = str(difficulty)
-    try:
-        url = f'http://www.eecs{data_dict["class"]}.org/self-grade-{hw_number}.html'
-    except:
-        print(f'{red}ERROR: Self-grade for this HW has either not released yet or this HW doesn\'t exist. Restarting.')
-    browser.visit(url)
-
-    for s in ['name', 'email', 'sid']:
-        browser.find_by_id(s).fill(data_dict[s])
-    close()
-
-    if data_dict['class'] == '16b':
-        browser.find_by_value(resubmission).click()
-
-    alt_tab()
-
-    inputs = browser.find_by_value('Comment')
-    indices = list(range(len(inputs)))
-    shuffle(indices)
-
-    print(f'There are {len(indices)} questions on this HW.')
-    print()
-
-    num_incorrects = int(input('How many questions out of these do you want to give an 8/10? ').strip())
-    print('I will now do your self-grade for you! I\'ll mark random questions as 8/10 and the rest as 10/10.')
-
-    print(yellow + 'IMPORTANT: Do not touch the keyboard or mouse until I am done.' + end)
-
-    print()
-    for i in range(5):
-        print(f'{cyan}Beginning self-grade in {5 - i} seconds.{end}', end='\r')
-        sleep(1)
-    print('\nSelf-grading now...')
-
-    alt_tab()
-
-    q, r = divmod(num_incorrects, len(data_dict['comments']))
-    comments = iter(q * data_dict['comments'] + data_dict['comments'][:r])
-
-    for i in indices[:-num_incorrects]:
-        browser.find_by_value('10')[i].click()
-
-    for i in indices[-num_incorrects:]:
-        browser.find_by_value('8')[i].click()
-        keyDown('tab')
-        write(next(comments))
-
-        
-    browser.find_by_id('d' + difficulty).click()
-    browser.find_by_id('Hours Spent').fill(hours_spent)
-    browser.find_by_id('Teammate Headcount').fill(num_teammates)
-    browser.find_by_id(went_to_hw_party).click()
-    browser.find_by_xpath('/html/body/section/form/p/button').click()
-    browser.find_by_id('json-download').click()
-
-    sleep(3)
-
-print()
-print('Submit the downloaded .json file to Gradescope and you\'re done! Have a great day!\n')
+print(f'Submit the selfgrades-{data_dict["hwNum"]}.json file in the out folder to Gradescope and you\'re done! Have a great day!\n')
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,13 @@
 import json
-from pyautogui import press, keyDown, keyUp, write
+import re
+from pyautogui import press, keyDown, keyUp
 from platform import system
+from time import sleep
+from splinter import Browser
+from fileinput import close
 
 cmd_alt = 'command' if system() == 'Darwin' else 'alt'
+wait_time = 3
 
 csi = '\x1B['
 red = csi + '31;1m'
@@ -11,6 +16,39 @@ green = csi + '32;1m'
 cyan = csi + '36;1m'
 magenta = csi + '95;1m'
 end = csi + '0m'
+
+def greet():
+    print('\nHello! My name is 16Bot, made by a lazy CS & Physics major named Shamith Pasula.')
+    print('I will be doing your 16A or 16B self-grade for you, giving you 8/10 on random questions to not be sus.')
+    print('The comments for the incorrect questions are in data.json, edit them if you wish.')
+    print('If this isn\'t your first time meeting me and you want to update your data, delete data.json and run the program again.')
+    print('If you mess up or want to restart, press Ctrl+C and run the program again. \n')
+
+def ask_user(default_comments):
+    try:
+        data_dict = open('bin/data.json', 'r')
+        data_dict = json.load(data_dict)
+        if 'comments' not in data_dict:
+            data_dict['comments'] = default_comments
+            with open('data.json', 'w') as data:
+                data.write(json.dumps(data_dict))
+        if 'class' not in data_dict:
+            print('Which class are you taking? Enter the number corresponding to your answer:')
+            print('1) EECS 16A')
+            print('2) EECS 16B')
+            data_dict['class'] = '16A' if input('Enter answer here: ').strip() == '1' else '16B'
+            with open('data.json', 'w') as data:
+                data.write(json.dumps(data_dict))
+            print()
+        print('I have your name, email, and SID already!\n')
+    except:
+        while True:
+            try:
+                print('I have a few questions for you:\n')
+                return get_data(default_comments)
+            except ValueError:
+                print(red + '\nERROR: Bad input. Restarting.\n' + end)
+    return data_dict
 
 def get_data(default_comments):
     print('Because this is the first time I met you, I need some of your information.\n')
@@ -37,21 +75,75 @@ def get_data(default_comments):
     
     return d
 
+def more_qs(data_dict):
+    while True:
+        try:
+            data_dict['hwNum'] = int(input('Which homework are you self-grading? ').strip())
+            if data_dict['hwNum'] < 10:
+                data_dict['hwNum'] = '0' + str(data_dict['hwNum'])
+            else:
+                data_dict['hwNum'] = str(data_dict['hwNum'])
+
+            if data_dict['class'] == '16b':
+                data_dict['resubmission'] = 'no'
+                while (data_dict['resubmission'] != 'Y' and data_dict['resubmission'] != 'N'):
+                    data_dict['resubmission'] = input('Is this a resubmission (Enter Y or N)? ').strip().upper()
+                data_dict['resubmission'] = 'yes' if data_dict['resubmission'] == 'Y' else 'no'
+
+            data_dict.update({
+                'Problem Set Difficulty': str(int(input(f'How difficult was HW {data_dict["hwNum"]} (Enter an integer between 1-10)? ').strip())),
+                'Hours Spent': str(int(input(f'How many hours did you spend on HW {data_dict["hwNum"]}? ').strip())),
+                'Teammate Headcount': str(int(input('How many people did you work with? ').strip())),
+                'Attended HW Party': input('Did you go to HW party (Enter Y or N)? ').strip().upper()
+            })
+
+            while (data_dict['Attended HW Party'] != 'Y' and data_dict['Attended HW Party'] != 'N'):
+                print(f'{red}Please enter Y or N.{end}')
+                data_dict['Attended HW Party'] = input('Did you go to HW party (Enter Y or N)? ').strip().upper()
+            break
+        except ValueError:
+            print(red + '\nERROR: Bad input. Restarting.\n' + end)
+        close()
+
 def alt_tab():
     keyDown(cmd_alt)
     press('tab')
     keyUp(cmd_alt)
 
+def get_inputs(data_dict):
+    print()
+    for i in range(wait_time):
+        print(f'{cyan}A new Google Chrome window will open in a new window in {wait_time - i} seconds, then I will navigate back to the terminal.{end}', end = '\r')
+        sleep(1)
+    print('\nOpening now...')
+
+    with Browser('chrome') as browser:
+        try:
+            url = f'http://www.eecs{data_dict["class"]}.org/self-grade-{data_dict["hwNum"]}.html'
+        except:
+            print(f'{red}ERROR: Self-grade for this HW has either not released yet or this HW doesn\'t exist. Restarting.{end}')
+        browser.visit(url)
+        alt_tab()
+        inputs = browser.find_by_value('Comment')
+
+        parts = [re.search(r'q(.+)-comment', el['for']).group(1) for el in inputs]
+    
+    return parts
+
 def get_parts(prompt, parts):
     p = input(prompt).strip().split(' ')
-    while not all(el in parts for el in p):
+    while not all(el in parts or not el for el in p):
         print(f'{red}ERROR: Question not in this HW. Try again.{end}')
-        p = input(prompt).strip().split(' ')
+        p = input(prompt).strip().lower().split(' ')
     return set(p)
 
-def fill(num, d, scores, comments):
+def infinite_gen(arr):
+    yield from arr
+    yield from infinite_gen(arr)
+
+def easy_fill(num, d, scores, comments):
     s = str(num)
     for p in scores[num]:
         d['q' + p] = s
-        if num < 10:
+        if 0 < num < 10:
             d[f'q{p}-comment'] = next(comments)
